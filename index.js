9f3b9567d9961cd36a64c2b67ff36afa6ada822b
spyder1265 <60445905+spyder1265@users.noreply.github.com>
2023-09-19 10:18:20 +0000

    improved algorithm

--- index.js
+++ index.js
@@ -1,54 +1,98 @@
-const express = require('express');
+const express = require("express");
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 const generatePassword = (len) => {
-  const length = len || 10;
-  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
-  let password = "";
+  const length = len || 12; // Default to a length of 12 characters
+  const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const digitChars = "0123456789";
+  const specialChars = "!@#$%^&*()_+[]{}|;:,.<>?";
 
-  for (let i = 0; i < length; i++) {
+  // Define character sets for each constraint
+  const constraints = [
+    lowercaseChars,
+    uppercaseChars,
+    digitChars,
+    specialChars,
+  ];
+
+  // Initialize an array to store characters from each constraint
+  const passwordArray = constraints.map((charset) => {
     const randomIndex = Math.floor(Math.random() * charset.length);
-    password += charset.charAt(randomIndex);
+    return charset.charAt(randomIndex);
+  });
+
+  // Generate the remaining characters
+  const remainingLength = length - passwordArray.length;
+  for (let i = 0; i < remainingLength; i++) {
+    const constraintIndex = Math.floor(Math.random() * constraints.length);
+    const charset = constraints[constraintIndex];
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    passwordArray.push(charset.charAt(randomIndex));
   }
 
-  return password;
+  // Shuffle the password to randomize character positions
+  for (let i = passwordArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [passwordArray[i], passwordArray[j]] = [passwordArray[j], passwordArray[i]];
+  }
+
+  return passwordArray.join("");
 };
 
-app.get('/', (req, res) => {
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
   const htmlResponse = `
-    <html lang="">
-      <head>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Password Generator</title>
         <style>
-        body{
-         background: #eef2fd;
+                         body {
+            background: #eef2fd;
         }
-          h1 {
+
+        h1 {
             color: #333;
             text-align: center;
-          }
-          h2 {
+            border-bottom: solid thin black;
+            line-height: normal;
+            padding-bottom: 10px;
+        }
+
+        h2 {
             color: #555;
-          }
+        }
+
         .copyright {
+            font-size: small;
             color: #999;
-            font-size: 12px;
             text-align: center;
             margin-top: 20px;
         }
-        footer{
-            position: fixed;
-            bottom: 0; 
-            width:100vw;
+
+        .Thanks {
+            text-align: center;
+            color: #999;
+            font-size: 12px;
         }
-        .usage{
+
+        .success {
+            color: green;
+        }
+
+        .usage {
             display: flex;
-            flex-direction: row;
-            padding:.7em .7em;
+            flex-direction: column;
+            padding: .7em .7em;
             border: 1px solid #ccc;
             border-radius: 5px;
-            margin: 10px 0 10px 0;
+            margin: 10px 0;
             background: #fff;
             box-shadow: 0 0 5px #ccc;
             font-size: 14px;
@@ -58,74 +102,158 @@ app.get('/', (req, res) => {
             word-wrap: break-word;
         }
 
-        .usage p {
-            padding : .6em;
+        .gen {
+            display: flex;
+            flex-direction: row;
+            padding: .7em .7em;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            margin: 10px 0;
+            background: #fff;
+            box-shadow: 0 0 5px #ccc;
+            align-items: center;
         }
 
-        .warning{
+        .gen input {
+            width: 400px;
+            height: 30px;
+        }
+
+        .gen button {
+            width: 100px;
+            height: 30px;
+            margin-left: 10px;
+            border-radius: 5px;
+            border: none;
+            background: #333;
+            color: #fff;
+            cursor: pointer;
+        }
+
+        .gen button:hover {
+            opacity: 0.7;
+        }
+
+        .warning {
             color: red;
             padding: 1.6em;
         }
 
+        
+#passwordResult {
+    width: 400px;
+    height: 30px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    padding: 5px;
+    margin-top: 10px;
+    font-family: monospace;
+}
 
-        h1{
-            border-bottom: solid thin black;
-            line-height: normal;
-            padding-bottom: 10px;
-        }
-        .copyright{
-            font-size: small;
-            color: #999;
-            text-align: center;
-            margin-top: 20px;
-        }
-        .Thanks{
-            text-align: center;
-            color: #999;
-            font-size: 12px;
-        }
-        .success{
-            color: green;
-        }
+.copy-button {
+    width: 100px;
+    height: 30px;
+    margin-left: 10px;
+    border-radius: 5px;
+    border: none;
+    background: #333;
+    color: #fff;
+    cursor: pointer;
+}
 
+.copy-button:hover {
+    opacity: 0.7;
+}
+
+.passwordgen{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
         </style>
-      </head>
-      <body>
-        <h1>Welcome to Password Generator API</h1>
-        <br>
+    </head>
+    <body>
+    <h1>Welcome to Password Generator API</h1>
+    <br>
 
-        <div class="usage">
-            <h2>Usage:</h2><p>http://passgen/gen/{length}</p><span class="warning"> Note: It can only be called through POST.</span>
+    <div class="usage">
+        <h2>Usage:</h2>
+        <p>http://passgen/gen/{length}</p>
+        <span class="warning"> Note: It can only be called through POST.</span>
+    </div>
+
+    <div class="usage">
+        <h2 class="success">Results:</h2>
+
+        <h4 class="success">Api response structure:</h4>
+        <p>{ "Password": password, "Length": password.length }</p>
+    </div>
+
+    <div class="gen">
+        <form>
+            <input type="number" min="12" value="12" class="input" id="passwordLengthInput" placeholder="Enter password length"/>
+            <button type="button" onclick="generatePassword()">Generate</button>
+            <p>generated password</p>
+                    <div class="passwordgen">
+        <input type="text" class="input" id="passwordResult" readonly/>
+        <button type="button" class="copy-button" onclick="copyPassword()">Copy</button>
         </div>
+        </form>
+    </div>
 
-
-
-        <div class="usage">
-            <h2 class="success">Results:</h2><p>{ "Password": password, "Length": password.length }</p>
+    <br>
+    <br>
+    <p class="Thanks">Thank You</p>
+    <footer>
+        <div class="copyright">
+            &copy; 2023 Spyder Hydra. All rights reserved.
         </div>
+    </footer>
 
-        <br>
-        <br>
-        <p class="Thanks">Thank You</p>
-        <footer>
-            <div class="copyright">
-                &copy; 2023 Spyder Hydra. All rights reserved.
-            </div>
-        </footer>
-      </body>
+    <script type="text/javascript">
+        function generatePassword() {
+            var passwordLengthInput = document.getElementById("passwordLengthInput");
+            var length = parseInt(passwordLengthInput.value);
+
+            if (isNaN(length) || length <= 0) {
+                alert("Please enter a valid password length.");
+                return;
+            }
+
+            var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var password = "";
+            for (var i = 0; i < length; i++) {
+                var randomIndex = Math.floor(Math.random() * charset.length);
+                password += charset.charAt(randomIndex);
+            }
+
+            var passwordResult = document.getElementById("passwordResult");
+            passwordResult.value = password;
+        }
+
+        function copyPassword() {
+            var passwordResult = document.getElementById("passwordResult");
+            passwordResult.select();
+            document.execCommand("copy");
+            alert("Password copied to clipboard: " + passwordResult.value);
+        }
+    </script>
+    </body>
     </html>
   `;
   res.send(htmlResponse);
 });
 
-app.post('/gen/:length', (req, res) => {
+app.post("/gen/:length", (req, res) => {
   const length = Number(req.params.length);
 
   if (isNaN(length) || length < 8) {
-    res.status(400).send({error: "Length should be a number greater than or equal to 8" });
+    res
+      .status(400)
+      .send({ error: "Length should be a number greater than or equal to 8" });
   } else {
     const password = generatePassword(length);
-    res.status(200).send({ "Password": password, "Length": password.length });
+    res.status(200).send({ Password: password, Length: password.length });
   }
 });
 
